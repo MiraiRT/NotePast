bb275f5fd839d291a1894a348d483195c0964d35
jlxrocket <triple._p@live.com>
2018-11-11 23:24:34 +0700

    add ui

--- src/fx/Main.java
+++ src/fx/Main.java
@@ -4,22 +4,30 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        Pane pane = new Pane();
+        Pane pane2 = new Pane();
+        Scene today = new Scene(pane,640,480);
+        Scene diary = new Scene(pane2,640,480);
+
+
+      //  VBox1.setManaged(false);
+
+        Parent root = FXMLLoader.load(getClass().getResource("today.fxml"));
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(root, 640, 480));
         primaryStage.show();
-
-
     }
 
 
-//    public static void main(String[] args) {
-//        launch(args);
-//    }
+    public static void main(String[] args) {
+        launch(args);
+    }
+
 }
