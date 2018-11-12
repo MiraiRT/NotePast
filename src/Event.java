ff733e9dae8f68b621e9047c1202f220c8d3a0d5
MiraiRT <59011114@kmitl.ac.th>
2018-11-12 21:24:02 +0700

    improve

--- src/Event.java
+++ src/Event.java
@@ -1,9 +1,49 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Event {
     private String time;
-    private String timeID;
+    private String eventID;
+    private String noteText;
+    private int numOfEvent = 0;
+    private List<String> eventTag;
 
-    public Event(String time,String timeID){
+
+
+    public Event(String time,String eventID,String noteText){
         this.time = time;
-        this.timeID = timeID;
+        this.eventID = eventID;
+        this.noteText = noteText;
+        this.eventTag = new ArrayList<>();
+        this.numOfEvent++;
     }
+
+    public String getNoteText() {
+        return noteText;
+    }
+
+    public void setNoteText(String noteText) {
+        this.noteText = noteText;
+    }
+
+    public int getEventID () {
+        int ID = 0;
+        return ID;
+    }
+
+    public boolean addEvent(DayPage todayPage){
+        // To do //
+        return false;
+    }
+
+    public boolean editEvent(DayPage todayPage){
+        // To do //
+        return false;
+    }
+
+    public boolean deleteEvent(DayPage todayPage){
+        // To do //
+        return false;
+    }
+
 }
